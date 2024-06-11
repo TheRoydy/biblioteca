cc53e89c22f83a4374869116347b47dc3ad632e5
Yordy Nuñez <yordierik05@gmail.com>
2024-06-11 13:15:29 -0500

    Se sube al repositorio el taller de biblioteca

---
+++ biblioteca_back_end/src/main/java/com/example/biblioteca_back_end/BibliotecaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.biblioteca_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaBackEndApplication.class, args);
+	}
+
+}
